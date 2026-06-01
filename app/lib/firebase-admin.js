1d4aa6b33d66a8fdf90b5a22fdef57fe64ba2ead
樂洋 <redna@sufeipo.localdomain>
2026-06-01 11:53:55 +0800

    change firebase to use json blob instead of env vars

--- app/lib/firebase-admin.js
+++ app/lib/firebase-admin.js
@@ -3,13 +3,8 @@ import { getAuth } from 'firebase-admin/auth';
 
 function getAdminAuth() {
   if (!getApps().length) {
-    initializeApp({
-      credential: cert({
-        projectId: process.env.FIREBASE_PROJECT_ID,
-        clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-        privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
-      }),
-    });
+    const sa = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
+    initializeApp({ credential: cert(sa) });
   }
   return getAuth();
 }
